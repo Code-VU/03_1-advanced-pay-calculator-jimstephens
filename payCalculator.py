b76068482b266ad6cdb2a2648c91784a84e8a466
Jim Stephens <Jim.Stephens@veteransunited.com>
2024-11-19 10:12:37 -0600

    WE3.2

--- payCalculator.py
+++ payCalculator.py
@@ -1,6 +1,21 @@
 def calculatePay():
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+    hours = input("Enter Hours: ")
+    pay_rate = input("Enter Rate: ")
+    try:
+        total_hours = float(hours)
+        total_rate = float(pay_rate)
+    except:
+        print("Error, please enter numeric input")
+        quit()
+
+    if total_hours > 40:
+        reg_pay = total_hours * total_rate
+        otp = (total_hours - 40.0) * (total_rate * 0.5)
+        total_pay = reg_pay + otp
+    else:
+        total_pay = total_hours * total_rate
+    print("Pay:",total_pay)
     
     # end assignment
 
